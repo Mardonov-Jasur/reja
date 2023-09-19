479f06397c9e0b5b11fcf44524c7c236c1cdd815
Mardonov-Jasur <jasur.mardonov97@gmail.com>
2023-09-19 15:36:27 +0900

    Fixed B and C task

--- train.js
+++ train.js
@@ -1,15 +1,39 @@
-function checkSimilarity(str1, str2) {
+       
+//++++++++++++++    B TASK   +++++++++++++//
 
-  for (let i = 0; i < str1.length; i++) {
-      for (let j = 0; j < str2.length; j++) {
-          if (str1[i] === str2[j]) {
-              return true;
-          }
+// function Sonlar(soz) {
+//     let position = 0;
+//     let count = 0;
+//     let number = "0123456789";
+//     for( position; position < soz.length; position++) {
+//       if (number.includes(soz.charAt(position))) {
+//         count++
+//       }
+//     }
+    
+//     return count;
+//   }
+  
+//   const result = Sonlar('bir2uch4b33esh6s');
+//   console.log(result);
+  
+
+
+ //++++++++++++    C TASK     ++++++++++++++++//
+ 
+function checkSimilarity(str1, str2) {
+    if (str1.length !== str2.length) {
+      return false; 
+    }
+  
+    for (let i = 0; i < str1.length; i++) {
+      if (str1[i] === str2[i]) {
+        return false; 
       }
+    }
+    
+    return true;
   }
   
-  return false; 
-}
-
-const result = checkSimilarity("entriw", "winter");
-console.log(result); 
+  const result = checkSimilarity("entriw", "winter");
+  console.log(result);
