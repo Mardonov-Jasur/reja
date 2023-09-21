1daf9d89c48f2425c9fc3055de2970f359973b97
Mardonov-Jasur <jasur.mardonov97@gmail.com>
2023-09-21 21:19:10 +0900

    Express freamwork & D TASK

--- account.js
+++ account.js
@@ -1,45 +1,45 @@
-const moment = require ("moment");
+// const moment = require ("moment");
 
-class Account {
-    #amount;
-    #account_id;
-    constructor (name, amount, account_id) {
-        this.name = name;
-        this.#amount = amount;
-        this.account_id = account_id;
-    }
+// class Account {
+//     #amount;
+//     #account_id;
+//     constructor (name, amount, account_id) {
+//         this.name = name;
+//         this.#amount = amount;
+//         this.account_id = account_id;
+//     }
 
-    tellMeBalance () {
-        console.log (`Sizning hisobingizdas=gi qoldiq ${this.#amount}`);
-        return this.#amount;
-    }
+//     tellMeBalance () {
+//         console.log (`Sizning hisobingizdas=gi qoldiq ${this.#amount}`);
+//         return this.#amount;
+//     }
 
-    withdrawMoney (amount) {
-        if(this.#amount > amount) {
-            this.#amount -= amount;
-            console.log (`Hisobdan ${amount} yechildi. Qoldiq ${this.#amount}`);
-        }else {
-            console.log (`Hisobingizda pul yetarli emas: ${this.#amount}`);
-        }
-    }
+//     withdrawMoney (amount) {
+//         if(this.#amount > amount) {
+//             this.#amount -= amount;
+//             console.log (`Hisobdan ${amount} yechildi. Qoldiq ${this.#amount}`);
+//         }else {
+//             console.log (`Hisobingizda pul yetarli emas: ${this.#amount}`);
+//         }
+//     }
 
-    makeDeposit(amount) {
-        this.#amount +=amount;
-        console.log (`Hisobingiz to'ldirildi: ${this.#amount}`);
-    }
+//     makeDeposit(amount) {
+//         this.#amount +=amount;
+//         console.log (`Hisobingiz to'ldirildi: ${this.#amount}`);
+//     }
 
-    giveMeDetails () {
-        console.log (`Salom ${this.name}! Sizning hisobingizda ${this.#amount}$ mavjud`);
-        console.log (`Hisob raqamingiz ${this.#account_id}`);
-    }
+//     giveMeDetails () {
+//         console.log (`Salom ${this.name}! Sizning hisobingizda ${this.#amount}$ mavjud`);
+//         console.log (`Hisob raqamingiz ${this.#account_id}`);
+//     }
 
-    static tellMeAboutClass () {
-        console.log ('Bu class bilan accountlar yasaladi');
-    }
+//     static tellMeAboutClass () {
+//         console.log ('Bu class bilan accountlar yasaladi');
+//     }
 
-    static tellMeAboutTime () {
-        console.log (`Hozirgi vaqt ${moment().format("YYYY-MM-DD:mm:ss")}`);
-    }
-}
+//     static tellMeAboutTime () {
+//         console.log (`Hozirgi vaqt ${moment().format("YYYY-MM-DD:mm:ss")}`);
+//     }
+// }
 
-module.exports = Account;
+// module.exports = Account;
--- train.js
+++ train.js
@@ -1,4 +1,43 @@
        
+
+//+++++++++++++++++++++++++++++  D TASK   +++++++++++++++++++++++++++++++++++++// 
+
+const moment = require ("moment")
+
+class Shop {
+    constructor(non, lagmon, cola) {
+        this.non = non;
+        this.lagmon = lagmon;
+        this.cola = cola;
+    }
+
+    Qoldiq() {
+        console.log(`${moment().format("HH:mm:ss")} Sotuvda ${this.non}ta non, ${this.lagmon}ta lagmon, ${this.cola}ta cola mavjud`);
+    }
+
+    Sotish(product, quantity) {
+        if (this[product] >= quantity) {
+            this[product] -= quantity;
+            console.log(`${moment().format("HH:mm:ss")} ${quantity}ta ${product} sotildi. Qoldiq ${this[product]}`);
+        } else {
+            console.log(`${moment().format("HH:mm:ss")} Bizda yetarli ${product} mavjud emas. Qoldiq ${this[product]}`);
+        }
+    }
+
+    Qabul(product, quantity) {
+        this[product] += quantity;
+        console.log(`${moment().format("YYY.MM.DD. HH:mm:ss")} ${quantity}ta ${product} qabul qilindi. Qoldiq ${this[product]}`);
+    }
+}
+
+const shop = new Shop(2, 4, 6);
+shop.Qoldiq();
+shop.Sotish('non', 1);
+shop.Qabul('lagmon', 3);
+
+
+
+
 //++++++++++++++    B TASK   +++++++++++++//
 
 // function Sonlar(soz) {
@@ -21,19 +60,21 @@
 
  //++++++++++++    C TASK     ++++++++++++++++//
  
-function checkSimilarity(str1, str2) {
-    if (str1.length !== str2.length) {
-      return false; 
-    }
+// function checkSimilarity(str1, str2) {
+//     if (str1.length !== str2.length) {
+//       return false; 
+//     }
   
-    for (let i = 0; i < str1.length; i++) {
-      if (str1[i] === str2[i]) {
-        return false; 
-      }
-    }
+//     for (let i = 0; i < str1.length; i++) {
+//       if (str1[i] === str2[i]) {
+//         return false; 
+//       }
+//     }
     
-    return true;
-  }
+//     return true;
+//   }
   
-  const result = checkSimilarity("entriw", "winter");
-  console.log(result);
+//   const result = checkSimilarity("entriw", "winter");
+//   console.log(result);
+
+
