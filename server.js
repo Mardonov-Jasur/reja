facd018e38a64a72adacfdfba279ace0b3bd3795
Mardonov-Jasur <jasur.mardonov97@gmail.com>
2023-09-25 20:57:19 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 console.log ('web serverni boshlash');
 const express = require ("express");
 const app = express();
-const http = require ('http')
+const http = require ('http');
+const fs = require ("fs");
+
+let user;
+fs.readFile ("database/user.json", "utf-8", (err,data) =>{
+    if(err) {
+        console.log ("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
 
 // 1 Kirish code
 app.use(express.static("public"));
@@ -16,16 +26,15 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4 Routing code
-// app.get("/hello", function (req, res) {
-//     res.end(`<h1>HELLO WORLD CUP</h1>`);
-// });
-// app.get("/gift", function (req, res) {
-//     res.end(`<h1>Siz sovgalar sahifasidasiz</h1>`);
-// });
 app.post("/create-item", (req, res) => {
     console.log(req.body);
     res.json({test: "success"});
 });
+
+app.get('/author', (req,res) => {
+    res.render("author", {user:user});
+})
+
 app.get("/", function (req, res) {
     res.render('harid');
 });
