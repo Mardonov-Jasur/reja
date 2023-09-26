25f9415da1ba31a4f2d9e0c0a5beb04614746535
Mardonov-Jasur <jasur.mardonov97@gmail.com>
2023-09-26 20:39:09 +0900

    18 EJS FREAMWORKIDA PORTFOLIO PUBLISH QILISH

--- train.js
+++ train.js
@@ -1,43 +1,43 @@
 
-console.log("Jack Ma maslahatlari");
-const list = [
-    "yaxshi talaba bo'ling", //0-20
-    "to'g'ri boshliq tanlang va ko'proq xato qiling", //20-30
-    "o'zingizni ishlaringizni boshlang", //30-40
-    "siz kuchli bo'lgan narsalarni qiling", //40-50
-    "yoshlarga investitsiya qiling", //50-60
-    "endi dam oling, fodasi yo'q", //60
-];
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//     "yaxshi talaba bo'ling", //0-20
+//     "to'g'ri boshliq tanlang va ko'proq xato qiling", //20-30
+//     "o'zingizni ishlaringizni boshlang", //30-40
+//     "siz kuchli bo'lgan narsalarni qiling", //40-50
+//     "yoshlarga investitsiya qiling", //50-60
+//     "endi dam oling, fodasi yo'q", //60
+// ];
 
 
-//Asynchronous function
-async function maslahatBering (a) {
-    if (typeof a !== "number") throw new Error("insert a number");
-    else if (a <= 20) return list[0];
-    else if (a > 20 && a <=30) return list[1];
-    else if (a > 30 && a <=40) return list[2];
-    else if (a > 40 && a <= 50) return list[3];
-    else if (a > 50 & a <= 60) return list[4];
-    else {
-        return new Promise ((resolve, reject) => {
-             setTimeout(() => {
-                resolve(list[5]);
-             }, 5000);
-        });
+// //Asynchronous function
+// async function maslahatBering (a) {
+//     if (typeof a !== "number") throw new Error("insert a number");
+//     else if (a <= 20) return list[0];
+//     else if (a > 20 && a <=30) return list[1];
+//     else if (a > 30 && a <=40) return list[2];
+//     else if (a > 40 && a <= 50) return list[3];
+//     else if (a > 50 & a <= 60) return list[4];
+//     else {
+//         return new Promise ((resolve, reject) => {
+//              setTimeout(() => {
+//                 resolve(list[5]);
+//              }, 5000);
+//         });
        
-    }
-}
+//     }
+// }
 
-//asyn/await
-async function run() {
-    let javob = await maslahatBering(65);
-    console.log(javob);
-    javob = await maslahatBering(31);
-    console.log(javob);
-    javob = await maslahatBering(41);
-    console.log(javob);
-}
-run();
+// //asyn/await
+// async function run() {
+//     let javob = await maslahatBering(65);
+//     console.log(javob);
+//     javob = await maslahatBering(31);
+//     console.log(javob);
+//     javob = await maslahatBering(41);
+//     console.log(javob);
+// }
+// run();
 
 
 //call via then/catch
