d69edea58a1699a9a2149383c8a9b72bcb1874d9
Mardonov-Jasur <jasur.mardonov97@gmail.com>
2023-09-28 22:18:24 +0900

    fix: refactoring

--- server.js
+++ server.js
@@ -36,11 +36,11 @@ app.get('/author', (req,res) => {
 })
 
 app.get("/", function (req, res) {
-    res.render('harid');
+    res.render('reja');
 });
 
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
-    console.log(`The server is running succesful om PORT ${PORT}`);
+    console.log(`The server is running succesful om PORT ${PORT}, http://localhost:${PORT}`);
 })
